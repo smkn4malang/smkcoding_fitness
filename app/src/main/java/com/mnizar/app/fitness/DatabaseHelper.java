3457488ad65e806faa88ac2f843ed2e956b5d9bb
mnizarzr <mnizarzr@gmail.com>
2018-09-26 01:24:49 +0700

    updt db

--- app/src/main/java/com/mnizar/app/fitness/DatabaseHelper.java
+++ app/src/main/java/com/mnizar/app/fitness/DatabaseHelper.java
@@ -7,7 +7,7 @@ import com.readystatesoftware.sqliteasset.SQLiteAssetHelper;
 public class DatabaseHelper extends SQLiteAssetHelper{
 
     private static final String DATABASE_NAME = "db_fitness.db";
-    private static final int DATABASE_VERSION = 1;
+    private static final int DATABASE_VERSION = 2;
 
     public DatabaseHelper(Context context){
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
